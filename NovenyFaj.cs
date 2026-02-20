b4f53f0a0731ac937084d08dfd34ee2491b9f274
hmarci <hecks.marton2@gmail.com@petrik.hu>
2026-02-20 09:27:41 +0100

    NovenyFaj osztaly kesz

--- NovenyFaj.cs
+++ NovenyFaj.cs
@@ -8,5 +8,35 @@ namespace UveghazProjekt
 {
 	internal class NovenyFaj
 	{
+		string nev;
+		int nedvessegtartalom;
+		int egeszsegszint;
+		int optimalissuruseg;
+
+		public NovenyFaj(string nev, int optimalissuruseg) 
+		{ 
+			this.nev = nev;
+			this.optimalissuruseg = optimalissuruseg;
+			this.nedvessegtartalom = 100;
+			this.egeszsegszint = 100;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Nedvessegtartalom { get => nedvessegtartalom; set => nedvessegtartalom = value; }
+		public int Egeszsegszint { get => egeszsegszint; set => egeszsegszint = value; }
+		public int Optimalissuruseg { get => optimalissuruseg; set => optimalissuruseg = value; }
+
+		public string Azonosito 
+		{ 
+			get
+			{
+				return this.nev.Substring(0, 3);
+			}
+		}
+
+		public override string ToString()
+		{
+			return $"{this.nev} nedvesseg: {this.nedvessegtartalom} egészség: {this.egeszsegszint} ideális egyedszam: {this.optimalissuruseg}";
+		}
 	}
 }
